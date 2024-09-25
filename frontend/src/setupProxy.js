36fd6edb119e15320205a34d7e2919acf0c89444
Jihana <wldnjs7618@naver.com>
2024-09-26 02:51:08 +0900

    proxy err

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app){
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'httpL//localhost:3001',
+            target: 'http://localhost:3001',
             changeOrigin: true,
         })
     )
