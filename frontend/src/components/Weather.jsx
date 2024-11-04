a570bd9cc13dd9fe669718a4bdeb3d19652c5d65
Jihana030 <wldnjs7618@naver.com>
2024-11-04 11:07:52 +0900

    api get, styled set, but defined err

--- frontend/src/components/Weather.jsx
+++ frontend/src/components/Weather.jsx
@@ -1,10 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import styled from 'styled-components';
 import WeatherDescKo from "./WeatherDescKo";
-
+const WeaderStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0;
+  span {
+    color: var(--natural);
+    font-size: 70px;
+    margin: 0 20px 0 10px;
+  }
+  div.weather-area {
+    font-size: 24px;
+    font-weight: 600;
+  }
+  div.weather-detail {
+    margin: 0 20px 0 20px;
+    font-weight: 600;
+    font-size: 28px;
+    :first-child {
+      color: var(--apricot);
+    }
+  }
+`;
 const Weather = () => {
   const API_KEY = process.env.REACT_APP_API_KEY;
-  const [loding, setLoding] = useState(false);
-  const [weather, setWeather] = useState();
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
@@ -14,26 +36,31 @@ const Weather = () => {
   }, []);
 
   const getWeather = async (lat, lon) => {
-    try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-      let res = await fetch(url);
-      setLoding(true);
-      let data = await res.json();
-      const weatherId = res.data.weather[0].id;
-      const weatherKo = WeatherDescKo[weatherId];
-      const weatherIcon = res.data.weather[0].icon;
-      const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-      const temp = Math.round(res.data.main.temp);
-
-      setWeather(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  return (
-    <div>{ weather}</div>
-  )
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data);
+    const weatherId = data.id;
+    const weatherKo = WeatherDescKo[weatherId];
+    const weatherIcon = data.icon;
+    const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+    const temp = Math.round(data.temp);
+    const wind = data.speed;
+    return (
+      <div>
+        <WeaderStyle>
+          <div className="weather-area">{weatherKo}</div>
+          <div className="weather-detail">
+            <div className="weather-temp">{temp}</div>
+            <div className="weather-wind">{wind}</div>
+          </div>
+          <div className="weather-icon">
+            <span className="material-symbols-rounded">{weatherIconAdrs}</span>
+          </div>
+        </WeaderStyle>
+      </div>
+    )
+  }
 }
 
 export default Weather;
